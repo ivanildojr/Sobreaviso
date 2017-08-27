8d818e443028ddd46ba2ad12942cf241d1fc8be0
rudsomlima <rudsomlima@gmail.com>
2017-08-27 06:59:52 -0300

    Gerando relatorio com horas diárias corretamente

--- grails-app/controllers/sobreavisonutel/RelatorioController.groovy
+++ grails-app/controllers/sobreavisonutel/RelatorioController.groovy
@@ -3,7 +3,6 @@ package sobreavisonutel
 import grails.plugin.springsecurity.annotation.Secured
 import grails.transaction.Transactional
 
-
 @Transactional(readOnly = true)
 @Secured('ROLE_ADMIN')
 class RelatorioController {
@@ -20,61 +19,71 @@ class RelatorioController {
         println params  //imprime tudo que foi retornado do formulario da view
 
         def atendente = params.list("atendente").get(0)  //recebe atendentes e dataInicio da view e tira da list
-        def dataInicio = params.list("dataInicio").get(0)
-        def dataFim = params.list("dataFim").get(0)
+        def stringDataInicio = params.list("dataInicio").get(0)
+        def stringDataFim = params.list("dataFim").get(0)
 
 //        println "dataInicio: " + dataInicio
 //        println "dataFim: $dataFim"
 //        println "atendente: $atendente"
 //        println "Parse: " + Date.parse("dd/MM/yyyy", dataInicio)
 //        println "Format: " + Date.parse("dd/MM/yyyy", dataInicio).format("yyyy-MM-dd")
-        dataInicio = Date.parse("dd/MM/yyyy", dataInicio).format("yyyy-MM-dd")              //passa a string datainicio pro formato de data, depois coloca na formatacao do banco
-        dataFim = Date.parse("dd/MM/yyyy", dataFim).format("yyyy-MM-dd")
+        Date dataInicio = Date.parse("dd/MM/yyyy", stringDataInicio)           //passa a string datainicio pro formato de data, depois coloca na formatacao do banco
+        Date dataFim = Date.parse("dd/MM/yyyy", stringDataFim)
 
         def atendenteId = Atendentes.findByNome(atendente)
         atendenteId = atendenteId.id
 
-        Calendar cal = Calendar.getInstance()
-        println cal.setTime(dataInicio)
-        cal.setFirstDayOfWeek (Calendar.SUNDAY);
-        int diaSemana = cal.get(Calendar.DAY_OF_WEEK);                       //pega o numero do dia de semana de 1:domingo a 7:sabado
-        cal.add (Calendar.DAY_OF_MONTH, Calendar.SUNDAY - diaSemana)
-//        println "dia da semana: " + Calendar.DAY_OF_WEEK
-        dataInicio = cal.getTime()                                          //pega a primeira semana da data inicial
-        def listBusca
-        while(dataFim >= dataInicio){
-            cal.add (Calendar.DAY_OF_MONTH, 6)
-            dataFim = cal.getTime()
-            listBusca << Historico.executeQuery("select dataEscala, hora from Historico where atendentes_id='$atendenteId' and dataEscala between '$dataInicio' and '$dataFim' and dataModificacao>=(select max(dataModificacao) from Historico where atendentes_id='$atendenteId') order by dataEscala")
+//        Calendar cal = Calendar.getInstance()
+//        cal.setTime(dataInicio)
+//        cal.setFirstDayOfWeek (Calendar.SUNDAY);
+//        int diaSemana = cal.get(Calendar.DAY_OF_WEEK);                       //pega o numero do dia de semana de 1:domingo a 7:sabado
+//        cal.add (Calendar.DAY_OF_MONTH, Calendar.SUNDAY - diaSemana)
+////        println "dia da semana: " + Calendar.SUNDAY - diaSemana
+//        dataInicio = cal.getTime()                                          //pega a primeira semana da data inicial
+//        dataInicio = cal.getTime()                                          //pega a primeira semana da data inicial
+        List listBusca = []
+        def busca
 
-            dataInicio = dataFim
+        while(dataFim > dataInicio){
+            stringDataInicio = dataInicio.format("yyyy-MM-dd").toString()
+            busca = Historico.executeQuery("select dataEscala, hora from Historico where dataEscala='$stringDataInicio' and dataModificacao>=(select max(dataModificacao) from Historico where atendentes_id='$atendenteId' and dataEscala='$stringDataInicio' ) order by dataEscala")
+
+            println "busca: " + busca
+            if(busca!=[]) listBusca << busca
+//            println "dataInico: " + dataInicio
+            dataInicio = dataInicio.plus(1)
         }
 
-        println listBusca
+        println "listBusca: " + listBusca
 
         def listData = []
         def listHora = []
-        def data
+        List list_i = []
+        Date data
         def horas = 0
         boolean flagData = 0
         def relatorio
         listBusca.each {i->
-//            println i
-            data = i[0]
+            println "i: "+ i
+            data = i[0][0]
+            list_i = i
+            horas = list_i.size()
+            println "listdata: " + listData
+
             if(!listData.contains(data)) {              //se a lista de datas ainda nao tem a data
                 listData << data                        //inclui data na listData
-                if(flagData) {                          //se passou por aqui pela 1 vez
+//                if(flagData) {                          //se passou por aqui pela 1 vez
                     listHora << horas
-//                    relatorio.hora = horas
-                }
-                horas = 0
-//                println "i_zero: " + data
-                horas++                               //inclui a primeira hora no somatorio de horas
-                flagData = 1;
+////                    relatorio.hora = horas
+//                }
+//                horas = 0
+////                println "i_zero: " + data
+//                horas++                               //inclui a primeira hora no somatorio de horas
+//                flagData = 1;
             }
             else {                                    //se já contem a data na listData, incrementa as horas desta data
                 horas++                              //busca quantas vezes a data aparece na lista, então essa é a soma de horas
-//                println "horas: " + horas
+                println "horas: " + horas
             }
         }
         listHora << horas
