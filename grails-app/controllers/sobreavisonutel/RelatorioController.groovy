283a5c8797b96edbd9d2d1aea16ecb33b905fb25
Rudsom <rudsomlima@gmail.com>
2017-09-01 16:04:45 -0300

    Relato de ocorrências editando e ordenando por data - OK - Iniciando relatório de horas trabalhadas v2

--- grails-app/controllers/sobreavisonutel/RelatorioController.groovy
+++ grails-app/controllers/sobreavisonutel/RelatorioController.groovy
@@ -3,6 +3,8 @@ package sobreavisonutel
 import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 import grails.transaction.Transactional
+import groovy.time.TimeCategory
+import groovy.time.TimeDuration
 
 @Transactional(readOnly = true)
 @Secured('ROLE_ADMIN')
@@ -45,21 +47,52 @@ class RelatorioController {
 //        dataInicio = cal.getTime()                                          //pega a primeira semana da data inicial
         List listBusca = []
         def busca
-        def horasTrabalhadas
+
 
 
         while(dataFim >= dataInicio){
             stringDataInicio = dataInicio.format("yyyy-MM-dd").toString()
             busca = Historico.executeQuery("select dataEscala, hora from Historico where dataEscala='$stringDataInicio' and dataModificacao>=(select max(dataModificacao) from Historico where atendentes_id='$atendenteId' and dataEscala='$stringDataInicio' ) order by dataEscala")
-            horasTrabalhadas = Ocorrencias.executeQuery("select data, horaInicio, horaFim from Ocorrencias")
 
-
-            println "horasTrabalhadas: " + horasTrabalhadas
             if(busca!=[]) listBusca << busca
 //            println "dataInico: " + dataInicio
             dataInicio = dataInicio.plus(1)
         }
 
+        /////////////////////////////////TRATANDO AS HORAS TRABALHADAS   /////////////////////////////////////////
+        def listDiasTrabalhados
+        def diaTrabalhado
+        List listHorasTrabalhadas = [], listDuracaoTrabalhadas = [], listHoraInicio = [], listHoraFim = []
+        String stringHoraInicio, stringHoraFim
+        Date horaInicio, horaFim
+        TimeDuration horasTrab
+        TimeDuration horasTrabTotal = new TimeDuration(0,0,0,0)
+
+        listDiasTrabalhados = Ocorrencias.executeQuery("select data, horaInicio, horaFim from Ocorrencias")
+//        println "diasTrabalhados: " + listDiasTrabalhados
+        listDiasTrabalhados.each {i->
+//            diaTrabalhado = i[0]
+//            println "diaTrabalhado: " + diaTrabalhado
+            stringHoraInicio = i[1]
+//            println "stringHoraInicio: " + stringHoraInicio
+            stringHoraFim = i[2]
+            horaInicio = Date.parse("yyyy-MM-dd HH:mm:ss", stringHoraInicio)
+            stringHoraInicio = horaInicio.getTimeString()
+            listHoraInicio << stringHoraInicio
+            horaFim = Date.parse('yyyy-MM-dd HH:mm:ss', stringHoraFim)
+            stringHoraFim = horaFim.getTimeString()
+            listHoraFim << stringHoraFim
+
+            horasTrab = TimeCategory.minus(horaFim, horaInicio)
+//            println "horasTrabalhadas: " + horasTrab
+            horasTrabTotal = horasTrab + horasTrabTotal
+            listHorasTrabalhadas << horasTrab
+        }
+        println "horasTrabTotal: " + horasTrabTotal
+        println "listHoraInicio: " + listHoraInicio
+        println "listHoraFim: " + listHoraFim
+
+        /////////////////////////////////TRATANDO AS HORAS EM SOBREAVISO/////////////////////////////////////////
         println "listBusca: " + listBusca
 
         def listData = []
@@ -74,7 +107,7 @@ class RelatorioController {
         boolean flagData = 0
         def relatorio
         listBusca.each {i->
-            println "i: "+ i
+//            println "i: "+ i
             data = i[0][0]
             list_i = i
             horas = list_i.size()                      //pega todas as horas do dia
@@ -83,10 +116,10 @@ class RelatorioController {
             periodoInicio = periodoInicio[1]
             periodoFim = list_i.get(horas-1)
             periodoFim = (periodoFim[1] as Integer) + 1
-            print "periodo: " + periodoInicio + " - "
-            println periodoFim
+//            print "periodo: " + periodoInicio + " - "
+//            println periodoFim
 
-            println "listdata: " + listData
+//            println "listdata: " + listData
 
             if(!listData.contains(data)) {              //se a lista de datas ainda nao tem a data
                 listData << data                        //inclui data na listData
@@ -95,13 +128,13 @@ class RelatorioController {
             }
         }
         listHora << horas
-        println listData
-        println listHora
+//        println listData
+//        println listHora
 
         List<Relatorio> relatorioList = new ArrayList<Relatorio>()
         listData.eachWithIndex {dia, index->
             data = listData.getAt(index)
-            println data
+//            println data
             relatorio = new Relatorio()
             relatorio.data = data
             relatorio.hora = listHora.getAt(index)
@@ -109,11 +142,12 @@ class RelatorioController {
             relatorioList.add(relatorio)
         }
 
-        println "horasTotal: " + horasTotal
+//        println "horasTotal: " + horasTotal
 
 
 
-        render(view: "index", model: [atendente:atendente, dataInicio:dataIni, dataFim:dataFim, listaBusca:relatorioList, horasTotal:horasTotal])
+        render(view: "index", model: [atendente:atendente, dataInicio:dataIni, dataFim:dataFim, listaBusca:relatorioList, horasTotal:horasTotal, listDiasTrabalhados: listDiasTrabalhados,
+                                      listDuracaoTrabalhadas: listDuracaoTrabalhadas])
 //        respond model: [listaBusca:relatorioList, horasTotal:horasTotal]
     }
 }
